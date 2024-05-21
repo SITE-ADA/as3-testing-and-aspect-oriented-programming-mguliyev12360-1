d1953c41652eb3962ff760618f7324e29aca2f10
mguliyev12360 <mguliyev12360@ada.edu.az>
2024-05-21 20:41:57 +0400

    first commit: demo and employeecrud created

---
+++ employeecrud/src/main/java/az/edu/ada/wm2/employeecrud/EmployeecrudApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.employeecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeecrudApplication.class, args);
+	}
+
+}
